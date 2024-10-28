da057b9c9b4cb9155915a6446fdc87d31c2a3edc
ZhengdanWang <zwang@slb.com>
2024-10-28 18:15:24 +0800

    feat: implement renderRows

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -50,9 +50,49 @@ export class MultiplicationTable {
     return expressionRows;
   }
 
+  public renderRows(table: MultiplicationExpression[][]): string {
+    let formattedTable = "";
+    let maxLength = 0;
+    table.forEach((row) => {
+      row.forEach(({ firstMultiplier, secondMultiplier, product }) => {
+        maxLength = Math.max(
+          maxLength,
+          firstMultiplier.toString().length,
+          secondMultiplier.toString().length,
+          product.toString().length
+        );
+      });
+    });
+
+    table.forEach((row) => {
+      let rowString = "";
+      row.forEach(({ firstMultiplier, secondMultiplier, product }) => {
+        const formattedFirst = firstMultiplier
+          .toString()
+          .padStart(maxLength, " ");
+        const formattedSecond = secondMultiplier
+          .toString()
+          .padStart(maxLength, " ");
+        const formattedProduct = product.toString().padStart(maxLength, " ");
+        rowString += `${formattedFirst} * ${formattedSecond} = ${formattedProduct}  `;
+      });
+      formattedTable += (rowString + '\n');
+    });
+    return formattedTable;
+  }
+
   public render(start: number, end: number): string {
     let renderReslut: string = "";
     if (!this.isValid(start, end)) return renderReslut;
+    
+    const expressionRows = this.getRows(start, end);
+
+    console.log(111);
+
+    renderReslut = this.renderRows(expressionRows);
+    console.log(222);
+
+    console.log(renderReslut);
     return renderReslut;
   }
 }
