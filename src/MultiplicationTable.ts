265456ae7889aa39bc984cffbc5aa092ee14c6b2
Kenan Gao <KGao6@slb.com>
2024-10-28 17:29:13 +0800

    feature:add multiplicationTable function

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -2,4 +2,10 @@ export class MultiplicationTable {
   public render(start: number, end: number): string{
     return '1*1=1'
   }
+
+  public multiplicationTable(startNum:number,endNumber:number):string{
+    let res=""
+    return res
+  }
+
 }
