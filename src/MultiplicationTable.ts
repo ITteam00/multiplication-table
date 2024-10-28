8f9b0aaff2d7eaf7b25ec12db0d6837670c8a78d
Xi Wu <XWu35@slb.com>
2024-10-28 18:21:31 +0800

    feat: add render function

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -1,50 +1,80 @@
 export class MultiplicationTable {
-  public render(start: number, end: number): string{
-    return '1*1=1';
-  }
-  public isCorrectOrder(start: number, end: number): boolean{
-    if(start > end)
-    {
+  public isCorrectOrder(start: number, end: number): boolean {
+    if (start > end) {
       return false;
     }
     return true;
   }
-  public isRangeInTen(validNum: number): boolean{
-    if(validNum < 1 || validNum >10)
-    {
+  public isRangeInTen(validNum: number): boolean {
+    if (validNum < 1 || validNum > 10) {
       return false;
     }
     return true;
   }
-  public isvalid(start: number, end: number): boolean{
-    if(this.isCorrectOrder(start, end) && 
-    this.isRangeInTen(start) &&
-    this.isRangeInTen(end))
-    {
+  public isvalid(start: number, end: number): boolean {
+    if (
+      this.isCorrectOrder(start, end) &&
+      this.isRangeInTen(start) &&
+      this.isRangeInTen(end)
+    ) {
       return true;
     }
     return false;
   }
 
-  public generateMultiplicationEquation(multiplerRow: number, multiplerColumn: number): string{
-    return `${multiplerRow}*${multiplerColumn}=${multiplerRow*multiplerColumn}`;
+  public generateMultiplicationEquation(
+    multiplerRow: number,
+    multiplerColumn: number
+  ): string {
+    return `${multiplerRow}*${multiplerColumn}=${
+      multiplerRow * multiplerColumn
+    }`;
   }
 
-  public generateMultiplicationRow(start: number, rowMultiplier: number): string[]{
+  public generateMultiplicationRow(
+    start: number,
+    rowMultiplier: number
+  ): string[] {
     let multiplicationRow: string[] = [];
-    for(let i = start; i <= rowMultiplier; i++){
-      let multiplicationNum: string = this.generateMultiplicationEquation(start, i);
+    for (let i = start; i <= rowMultiplier; i++) {
+      let multiplicationNum: string = this.generateMultiplicationEquation(
+        start,
+        i
+      );
       multiplicationRow.push(multiplicationNum);
     }
     return multiplicationRow;
   }
 
-  public generateMultiplicationTable(start: number, end: number): string[][]{
+  public generateMultiplicationTable(start: number, end: number): string[][] {
     let multiplicationTable: string[][] = [];
-    for(let i = start; i <= end; i++){
-      let multiplicationRow: string[] = this.generateMultiplicationRow(start, i);
+    for (let i = start; i <= end; i++) {
+      let multiplicationRow: string[] = this.generateMultiplicationRow(
+        start,
+        i
+      );
       multiplicationTable = [...multiplicationTable, multiplicationRow];
     }
     return multiplicationTable;
-}
+  }
+  public renderMultiplicationTable(start: number, end: number): string {
+    if (!this.isvalid(start, end)) {
+      return "";
+    }
+    
+  }
+
+  public render(multiplicationTable: string[][]): string {
+    let table: string = "";
+    for (let i = 0; i < multiplicationTable.length; i++) {
+      for (let j = 0; j < multiplicationTable[i].length; j++) {
+        table += multiplicationTable[i][j];
+        table += "\t";
+        if (j < multiplicationTable[i].length - 1) {
+          table += "\n";
+        }
+      }
+    }
+    return table;
+  }
 }
