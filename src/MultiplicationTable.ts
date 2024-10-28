54f72642dafd7f36cd693a0eb4102171dc0bc466
ZhengdanWang <zwang@slb.com>
2024-10-28 17:03:40 +0800

    feat: add checkValid

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -1,5 +1,18 @@
 export class MultiplicationTable {
+  public inBoundary(value: number): boolean {
+    return value >= 1 && value <= 10;
+  }
+  
+  public isValid(start: number, end: number): boolean {
+    if (start > end) return false;
+    if (!this.inBoundary(start) || !this.inBoundary(end)) {
+      return false;
+    }
+    return true;
+  }
   public render(start: number, end: number): string{
-    return '1*1=1'
+    let renderReslut: string = "";
+    if (this.isValid(start, end)) return renderReslut; 
+    return renderReslut;
   }
 }
