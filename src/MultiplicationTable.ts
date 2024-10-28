8b2f978f251fd57fa04461ce9448895c89010318
Kenan Gao <KGao6@slb.com>
2024-10-28 17:45:35 +0800

    feature: add function signature

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -3,9 +3,36 @@ export class MultiplicationTable {
     return '1*1=1'
   }
 
-  public multiplicationTable(startNum:number,endNumber:number):string{
-    let res=""
-    return res
+  public isValidInput(startNum:number,endNum:number):boolean{
+    if (this.isValidNumber(startNum)&&this.isValidNumber(endNum)&&this.isValidRange(startNum,endNum)){
+      return true
+    }
+    else{
+      return false
+    }
   }
+
+  public isValidNumber(num:number):boolean{
+
+  }
+
+  public isValidRange(startNum:number,endNum:number):boolean{
+
+  }
+
+  public multiplicationTable(startNum:number,endNum:number):string[]{
+    let res:string[]=[]
+    if(this.isValidInput(startNum,endNum)){
+      for(let i=startNum;i++;i<=endNum){
+        for(let j=i;j++;j<=endNum)
+        res.push(this.multiplicationRow(i,j))
+      }
+    }
+  }
+
+  public multiplicationRow(startNum:number,endNumber:number):string{
+
+  }
+
 
 }
