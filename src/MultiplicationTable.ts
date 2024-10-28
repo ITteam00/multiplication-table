dffed329ad3b63c87c2fb8e9b5c0c2f1d74a669e
zjiang10 <zjiang10@slb.com>
2024-10-28 18:38:08 +0800

    feat: added validateInputs

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -1,7 +1,19 @@
 export class MultiplicationTable {
 
   public validateInputs(start: any, end: any): boolean {
-
+    if (!(start && end)) {
+      return false;
+    }
+    if (!(Number.isInteger(start) && Number.isInteger(end))) {
+      return false;
+    }
+    if (start < 0 || start > 10 || end < 0 || end > 10) {
+      return false;
+    }
+    if (start > end) {
+      return false;
+    }
+    return true;
   }
 
   public generateFromIndex(start: number, main: number): string[] {
@@ -14,7 +26,10 @@ export class MultiplicationTable {
 
 
   public render(start: number, end: number): string {
-
+    if (!this.validateInputs(start, end)) {
+      return "";
+    }
+    return ""
   }
 
 
