eed1a5f9d9bc42af3f79b6a0cf44120c06b36338
yyu26 <yyu26@slb.com>
2024-10-28 16:48:31 +0800

    add isValid() method

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -1,5 +1,9 @@
 export class MultiplicationTable {
-  public render(start: number, end: number): string{
-    return '1*1=1'
+  public render(start: number, end: number): string {
+    return "1*1=1";
+  }
+
+  public isValid(start: number, end: number): boolean {
+    return start >= 1 && start <= 10 && end >= 1 && end <= 10 && start <= end;
   }
 }
