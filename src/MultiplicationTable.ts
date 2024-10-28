cf808bf7f76b156c686ae1a353e7df3d1bd9566a
kgaohahaha <15296612347@163.com>
2024-10-28 18:29:25 +0800

    feature: add data valiadation

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -1,6 +1,5 @@
 export class MultiplicationTable {
   public render(start: number, end: number): string{
-    return '1*1=1'
   }
 
   public isValidInput(startNum:number,endNum:number):boolean{
@@ -13,26 +12,30 @@ export class MultiplicationTable {
   }
 
   public isValidNumber(num:number):boolean{
+    if(Number.isInteger(num)&&num>=1&&num<=10){
+      return true
+    }
+    else{
+      return false
+    }
 
   }
 
   public isValidRange(startNum:number,endNum:number):boolean{
-
-  }
-
-  public multiplicationTable(startNum:number,endNum:number):string[]{
-    let res:string[]=[]
-    if(this.isValidInput(startNum,endNum)){
-      for(let i=startNum;i++;i<=endNum){
-        for(let j=i;j++;j<=endNum)
-        res.push(this.multiplicationRow(i,j))
-      }
+    if(startNum<=endNum){
+      return true
     }
+    else{
+      return false
+    }
+
   }
 
-  public multiplicationRow(startNum:number,endNumber:number):string{
+  public multiplicationTable(startNum:number,endNum:number):string[][]{
 
   }
 
+  public multiplicationRow(startNum:number,endNumber:number):string[]{
 
+  }
 }
