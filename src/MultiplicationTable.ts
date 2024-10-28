4d308aa07a4487d3c49e6b1e6c92e1b8a5b3e7fd
Xi Wu <XWu35@slb.com>
2024-10-28 16:48:23 +0800

    feat: add IsCorrectOrder function

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -1,5 +1,12 @@
 export class MultiplicationTable {
   public render(start: number, end: number): string{
-    return '1*1=1'
+    return '1*1=1';
+  }
+  public IsCorrectOrder(start: number, end: number): bool{
+    if(start > end)
+    {
+      return false;
+    }
+    return true;
   }
 }
